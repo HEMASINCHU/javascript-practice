813add12043e775c3d20cdc4c29e721f77e9b874
R Hema <ramakrishnagarihema@gmail.com>
2023-08-26 00:13:01 +0530

    fix the timer function

--- quizz/script.js
+++ quizz/script.js
@@ -2,19 +2,19 @@ const questions = [
   {
     text: "What is JS stands for?",
     options: ["JavaScript", "Java", "Python", "Angular"],
-    anwerkey: 0,
+    expectedKey: 0,
     actualKey: 1,
   },
   {
     text: "What is Array data type",
     options: ["mixed datatype", "beta", "alpha", "gamma"],
-    anwerkey: 0,
+    expectedKey: 0,
     actualKey: 1,
   },
   {
     text: "What is Object data type",
-    options: ["mixed datatype", "beta", "alpha", "gamma"],
-    exceptedKey: 0,
+    options: ["collection of properties", "beta", "alpha", "gamma"],
+    expectedKey: 0,
     actualKey: 1,
   },
 ];
@@ -22,7 +22,7 @@ const questions = [
 let totalQuestions = questions.length;
 let currentQuestionIndex = 0;
 let score = 0;
-const MIN_SCORE = 3;
+const MIN_SCORE = 1;
 
 const questionText = document.getElementById("question-text");
 const questionNumber = document.getElementById("question-number");
@@ -40,13 +40,13 @@ function updateTimerDisplay() {
 
 function startTimer() {
   timerInterval = setInterval(() => {
-    timerCount--;
-    updateTimerDisplay();
-
-    const timeLimitInSeconds = 0;
-    if (timerCount <= timeLimitInSeconds) {
-      alert("Time's up!");
+    if (timerCount > 0) {
+      timerCount--;
+      updateTimerDisplay();
+    } else {
       stopTimer();
+      alert("Time's up!");
+      moveToNextQuestion();
     }
   }, 1000);
 }
@@ -56,16 +56,15 @@ function stopTimer() {
 }
 
 function loadQuestion(questionIndex) {
+  stopTimer();
   const question = questions[questionIndex];
   questionText.textContent = question.text;
   for (let i = 0; i < options.length; i++) {
     options[i].value = i;
-    options[i].checked = false;
+    options[i].checked = i == question.actualKey;
     options[i].nextSibling.textContent = question.options[i];
   }
   updateQuestionNumber();
-  stopTimer();
-  timerCount = 30;
   updateTimerDisplay();
   startTimer();
 }
@@ -73,15 +72,16 @@ function loadQuestion(questionIndex) {
 function updateQuestionNumber() {
   questionNumber.textContent = `Question ${
     currentQuestionIndex + 1
-  } of  ${totalQuestions}`;
+  } of ${totalQuestions}`;
 }
 
 previousBtn.disabled = true;
 
 function checkAnswer(selectedIndex) {
-  if (selectedIndex === questions[currentQuestionIndex].anwerkey) {
+  if (selectedIndex === questions[currentQuestionIndex].expectedKey) {
     score++;
   }
+  questions[currentQuestionIndex].actualKey = selectedIndex;
 }
 
 function showResult() {
@@ -94,14 +94,48 @@ function showResult() {
   `;
 }
 
+function moveToNextQuestion() {
+  stopTimer();
+  timerCount = 30;
+
+  if (currentQuestionIndex === totalQuestions - 1) {
+    if (score >= MIN_SCORE) {
+      showResult();
+    } else {
+      alert("You did not score enough points.");
+    }
+  } else {
+    currentQuestionIndex++;
+    loadQuestion(currentQuestionIndex);
+    previousBtn.disabled = false;
+    nextBtn.disabled = false;
+
+    if (currentQuestionIndex === 0) {
+      previousBtn.disabled = true;
+    }
+
+    if (currentQuestionIndex === totalQuestions - 1) {
+      nextBtn.textContent = "Submit";
+    } else {
+      nextBtn.textContent = "Next";
+    }
+  }
+}
+
 previousBtn.addEventListener("click", () => {
   if (currentQuestionIndex > 0) {
     currentQuestionIndex--;
+    timerCount = 30;
     loadQuestion(currentQuestionIndex);
     nextBtn.disabled = false;
-  }
-  if (currentQuestionIndex === 0) {
-    previousBtn.disabled = false;
+
+    if (currentQuestionIndex === 0) {
+      previousBtn.disabled = true;
+    }
+
+    if (currentQuestionIndex !== totalQuestions - 1) {
+      nextBtn.textContent = "Next";
+    }
   }
 });
 
@@ -111,30 +145,10 @@ nextBtn.addEventListener("click", () => {
   if (selectedOption) {
     const selectedIndex = +selectedOption.value;
     checkAnswer(selectedIndex);
-
-    if (currentQuestionIndex === totalQuestions - 1) {
-      if (score >= MIN_SCORE) {
-        showResult();
-      } else {
-        alert("You did not score 3 or more points.");
-      }
-    } else {
-      currentQuestionIndex++;
-      loadQuestion(currentQuestionIndex);
-      previousBtn.disabled = false;
-      nextBtn.disabled = false;
-
-      if (currentQuestionIndex === totalQuestions - 1) {
-        nextBtn.textContent = "Submit";
-      }
-    }
+    moveToNextQuestion();
   } else {
     alert("Please select an option before proceeding.");
   }
 });
 
 loadQuestion(currentQuestionIndex);
-startTimer();
-
-previousBtn.disabled = true;
-updateQuestionNumber();
