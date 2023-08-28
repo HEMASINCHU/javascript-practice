152c6180c52a6154603bff1557fcf477916c8f71
R Hema <ramakrishnagarihema@gmail.com>
2023-08-28 15:50:53 +0530

    fix the comments on toggle button

--- quizz/script.js
+++ quizz/script.js
@@ -135,14 +135,7 @@ themeToggleBtn.addEventListener("click", toggleTheme);
 
 function toggleTheme() {
   const body = document.body;
-
-  if (body.classList.contains("light-theme")) {
-    body.classList.remove("light-theme");
-    body.classList.add("dark-theme");
-  } else {
-    body.classList.remove("dark-theme");
-    body.classList.add("light-theme");
-  }
+  body.classList.toggle("dark-theme");
 }
 
 toggleTheme();
